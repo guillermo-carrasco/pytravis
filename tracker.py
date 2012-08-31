ce20edaaf6f39c4a1667a0e58644f097d0be954c
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-08-31 18:38:10 +0200

    Added raw adresses for the travis API. Created some connections with the UI

--- tracker.py
+++ tracker.py
@@ -1,17 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import pygtk
 pygtk.require("2.0")
 
 import gtk
 import gtk.glade
+import urllib
+import json
 
-class MainWin:
+########################
+# Travis API addresses #
+########################
+
+repos = 'http://travis-ci.org/repositories.json'
+reposByOwner = 'http://travis-ci.org/repositories.json?owner_name=:owner'
+reposIndexSearch = 'http://travis-ci.org/repositories.json?search=:query'
+reposShow = 'http://travis-ci.org/:owner_name/:name.json'
+builds = 'http://travis-ci.org/:owner_name/:name/builds.json'
+buildsShow = 'http://travis-ci.org/:owner_name/:name/builds/:id.json'
+workers = 'http://travis-ci.org/workers.json'
+jobs = 'http://travis-ci.org/jobs.json'
+jobsShow = 'http://travis-ci.org/jobs/:id.json'
+
+
+class TravisCIUsername:
 
     def __init__(self):
-        self.widgets = gtk.glade.XML("mainWindow.glade")
-        signals = { \
-            "on_entri1_activate" : self.on_button1_clicked, \
-            "on_button1_clicked" : self.on_button1_clicked,
-            "gtk_main_quit" : gtk.main_quit
-        }
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("UI/mainWindow.glade")
 
-        self.widgets.signal_autoconnect(signals)
+        self.mainWindow = self.builder.get_object("window1")
+        self.okButton = self.builder.get_object("okButton")
+        self.travisName = self.builder.get_object("entryUser")
+        self.builder.connect_signals(self)
+
+    def on_okButton_clicked(self, widget):
+        builds = json.loads(urllib.urlopen(reposByOwner.replace(':owner', self.travisName.get_text())).read())
+        if not builds:
+            error = ErrorUserName()
+            error.dialog.show()
+        else:
+            s = ShoWBuilds()
+            s.window.show()
+
+    def gtk_main_quit(self, widget):
+        gtk.main_quit()
+
+
+class ErrorUserName:
+
+    def __init__(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("UI/noUser.glade")
+
+        self.dialog = self.builder.get_object("messagedialog1")
+        self.backButton = self.builder.get_object("backButton")
+        self.builder.connect_signals(self)
+
+    def on_backButton_clicked(self, widget):
+        self.dialog.destroy()
+
+
+class ShoWBuilds:
+
+    def __init__(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("UI/showBuilds.glade")
+
+        self.window = self.builder.get_object("window1")
+        self.listOfBuilds = self.builder.get_object("buildsList")
+        self.listOfBuilds.append(('blabla',''))
+
+
+    def gtk_main_quit(self, widget):
+        gtk.main_quit()
+
+if __name__ == "__main__":
+    a = TravisCIUsername()
+    a.mainWindow.show()
+    gtk.main()
