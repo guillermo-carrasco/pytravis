ec755f0c767428db5ce76219141acbee6df0d85e
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-09-03 09:47:10 +0200

    Several improvements: Showing builds list sorted by repo, working in the jobs list.

--- tracker.py
+++ tracker.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import pygtk
 pygtk.require("2.0")
 
 import gtk
 import gtk.glade
-import urllib
-import json
-
-########################
-# Travis API addresses #
-########################
-
-repos = 'http://travis-ci.org/repositories.json'
-reposByOwner = 'http://travis-ci.org/repositories.json?owner_name=:owner'
-reposIndexSearch = 'http://travis-ci.org/repositories.json?search=:query'
-reposShow = 'http://travis-ci.org/:owner_name/:name.json'
-builds = 'http://travis-ci.org/:owner_name/:name/builds.json'
-buildsShow = 'http://travis-ci.org/:owner_name/:name/builds/:id.json'
-workers = 'http://travis-ci.org/workers.json'
-jobs = 'http://travis-ci.org/jobs.json'
-jobsShow = 'http://travis-ci.org/jobs/:id.json'
+import travisParser
 
 
 class TravisCIUsername:
@@ -29,19 +13,19 @@ class TravisCIUsername:
     def __init__(self):
         self.builder = gtk.Builder()
         self.builder.add_from_file("UI/mainWindow.glade")
+        self.builder.connect_signals(self)
 
         self.mainWindow = self.builder.get_object("window1")
         self.okButton = self.builder.get_object("okButton")
         self.travisName = self.builder.get_object("entryUser")
-        self.builder.connect_signals(self)
 
     def on_okButton_clicked(self, widget):
-        builds = json.loads(urllib.urlopen(reposByOwner.replace(':owner', self.travisName.get_text())).read())
+        builds, jobs = travisParser.getBuildsAndJobs(self.travisName.get_text())
         if not builds:
             error = ErrorUserName()
             error.dialog.show()
         else:
-            s = ShoWBuilds()
+            s = ShoWBuildsAndJobs(builds, jobs)
             s.window.show()
 
     def gtk_main_quit(self, widget):
@@ -53,28 +37,72 @@ class ErrorUserName:
     def __init__(self):
         self.builder = gtk.Builder()
         self.builder.add_from_file("UI/noUser.glade")
+        self.builder.connect_signals(self)
 
         self.dialog = self.builder.get_object("messagedialog1")
         self.backButton = self.builder.get_object("backButton")
-        self.builder.connect_signals(self)
 
     def on_backButton_clicked(self, widget):
         self.dialog.destroy()
 
 
-class ShoWBuilds:
+class ShoWBuildsAndJobs():
 
-    def __init__(self):
+    def __init__(self, builds, jobs):
         self.builder = gtk.Builder()
         self.builder.add_from_file("UI/showBuilds.glade")
+        self.builder.connect_signals(self)
 
         self.window = self.builder.get_object("window1")
-        self.listOfBuilds = self.builder.get_object("buildsList")
-        self.listOfBuilds.append(('blabla',''))
+        self.buildsTree = self.builder.get_object("treeBuildsList")
+        self.jobsTree = self.builder.get_object("treeJobsList")
+        self._addColumn("Repository", 0, self.buildsTree)
+        self._addColumn("Build ID", 1, self.buildsTree)
+        self._addColumn("Build Number", 2, self.buildsTree)
+        self._addColumn("State", 3, self.buildsTree)
+        self.buildsList = self.builder.get_object("buildsList")
+
+        for elem in builds:
+            self._addBuild(elem, self.buildsList)
+
+        self._addColumn("Repository", 0, self.jobsTree)
+        self._addColumn("Job ID", 1, self.jobsTree)
+        self._addColumn("Job Number", 2, self.jobsTree)
+        self._addColumn("Status", 3, self.jobsTree)
+        self.jobsList = self.builder.get_object("jobsList")
+
+        for job in jobs:
+            self._addJob(job, self.jobsList)
+
+
+    def _addColumn(self, title, columnID, tree):
+        """
+        This function adds a column to the list view. First it create the gtk.
+        TreeViewColumn and then set some needed properties.
+        """
+        column = gtk.TreeViewColumn(title, gtk.CellRendererText(), text = columnID)
+        column.set_resizable(True)
+        column.set_sort_column_id(columnID)
+        tree.append_column(column)
+
+
+    def _addBuild(self, build, listview):
+        """
+        Add build to the tree view
+        """
+        listview.append([build['repository'], int(build['id']), int(build['number']), build['state']])
+
+
+    def _addJob(self, job, listview):
+        """
+        Add a job to the tree view
+        """
+        listview.append([job['repository'], int(job['id']), int(job['number']), job['status']])
 
 
     def gtk_main_quit(self, widget):
         gtk.main_quit()
+
 
 if __name__ == "__main__":
     a = TravisCIUsername()
---
+++ travisParser.py
@@ -0,0 +1,43 @@
+import json
+import urllib
+
+########################
+# Travis API addresses #
+########################
+urlBase = 'http://travis-ci.org/'
+
+repos = urlBase + 'repositories.json'
+reposByOwner = urlBase + 'repositories.json?owner_name={owner}'
+reposIndexSearch = urlBase + 'repositories.json?search={query}'
+reposShow = urlBase + '{owner_name}/{name}.json'
+builds = urlBase + '{owner_name}/{name}/builds.json'
+buildsShow = urlBase + '{owner_name}/{name}/builds/{id}.json'
+workers = urlBase + 'workers.json'
+jobs = urlBase + 'jobs.json'
+jobsShow = urlBase + 'jobs/{id}.json'
+
+
+def getBuildsAndJobs(_owner):
+    """
+    Returns a list with all the owner's builds
+    """
+    repos = json.loads(urllib.urlopen(reposByOwner.format(owner=_owner)).read())
+    buildsList = []
+    runningList = []
+    jobsList = []
+
+    # Build the builds list
+    for repo in repos:
+        buildsPerRepo = json.loads(urllib.urlopen(builds.format(owner_name=_owner, name=repo['slug'].split('/')[-1])).read())
+        for build in buildsPerRepo:
+            build['repository'] = repo['slug'].split('/')[-1]
+            if build['state'] == 'started':
+                runningList.append(build)
+            buildsList.append(build)
+
+    # Build running jobs list
+    for r in runningList:
+        jobID = json.loads(urllib.urlopen(buildsShow.format(owner_name=_owner, name=r['repository'], id=r['id'])).read())['matrix'][0]['id']
+        jobsList.append(json.loads(urllib.urlopen(jobs.format(id = jobID)).read()))
+
+    return buildsList, jobsList
