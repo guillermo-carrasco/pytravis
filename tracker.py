ae3d7cff417f0d1ff116a8ca14c48742137a6e06
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-11-02 16:58:52 +0100

    Removed all GUI stuff. Starting to write the new tracker

--- tracker.py
+++ tracker.py
@@ -1,163 +1,18 @@
 #!/usr/bin/env python
 
-import pygtk
-pygtk.require("2.0")
+import traivs
 
-import gtk
-import gtk.glade
-import gtk.gdk
-import travisParser
-import codecs
+t = travis.TravisManager()
 
+def _getLog(owner, repo=None, jobID=None):
+    """Save a log to a file. 
+        If repo and jobID are specified, then the log is directly saved to disk (if no errors). Otherwise, the program ask the user until
+        all the necessary information has been obtained.
+    """
 
-class TravisCIUsername:
+def _followLog():
+    """Keeps printing a non-finished build's log.
+    """
 
-    def __init__(self):
-        self.builder = gtk.Builder()
-        self.builder.add_from_file("UI/mainWindow.glade")
-        self.builder.connect_signals(self)
-
-        self.mainWindow = self.builder.get_object("window1")
-        self.okButton = self.builder.get_object("okButton")
-        self.travisName = self.builder.get_object("entryUser")
-
-    def on_okButton_clicked(self, widget):
-        builds, jobs = travisParser.getBuildsAndJobs(self.travisName.get_text())
-        if not builds:
-            error = ShowDialog("Error!", "This username doesn't appear to be valid.")
-            error.dialog.show()
-        else:
-            s = ShoWBuildsAndJobs(builds, jobs, self.travisName.get_text())
-            s.window.show()
-
-    def gtk_main_quit(self, widget):
-        gtk.main_quit()
-
-
-class ShowDialog:
-
-    def __init__(self, markup, secondaryText):
-        self.builder = gtk.Builder()
-        self.builder.add_from_file("UI/noUser.glade")
-        self.builder.connect_signals(self)
-
-        self.dialog = self.builder.get_object("messagedialog1")
-        self.dialog.set_markup(markup)
-        self.dialog.format_secondary_text(secondaryText)
-
-    def on_backButton_clicked(self, widget):
-        self.dialog.destroy()
-
-
-class ShoWBuildsAndJobs():
-
-    def __init__(self, builds, jobs, owner):
-        self.builder = gtk.Builder()
-        self.builder.add_from_file("UI/showBuilds.glade")
-        self.builder.connect_signals(self)
-        self.owner = owner
-
-        self.window = self.builder.get_object("window1")
-        self.buildsTree = self.builder.get_object("treeBuildsList")
-        self.jobsTree = self.builder.get_object("treeJobsList")
-        self._addColumn("Repository", 0, self.buildsTree)
-        self._addColumn("Build ID", 1, self.buildsTree)
-        self._addColumn("Build Number", 2, self.buildsTree)
-        self._addColumn("State", 3, self.buildsTree)
-        self.buildsList = self.builder.get_object("buildsList")
-
-        for elem in builds:
-            self._addBuild(elem, self.buildsList)
-
-        self._addColumn("Repository", 0, self.jobsTree)
-        self._addColumn("Job ID", 1, self.jobsTree)
-        self._addColumn("Job Number", 2, self.jobsTree)
-        self.jobsList = self.builder.get_object("jobsList")
-
-        for job in jobs:
-            self._addJob(job, self.jobsList)
-
-
-    def _addColumn(self, title, columnID, tree):
-        """
-        This function adds a column to the list view. First it create the gtk.
-        TreeViewColumn and then set some needed properties.
-        """
-        column = gtk.TreeViewColumn(title, gtk.CellRendererText(), text = columnID)
-        column.set_resizable(True)
-        column.set_sort_column_id(columnID)
-        tree.append_column(column)
-
-
-    def _addBuild(self, build, listview):
-        """
-        Add build to the tree view
-        """
-        listview.append([build['repository'], int(build['id']), int(build['number']), build['state']])
-
-
-    def _addJob(self, job, listview):
-        """
-        Add a job to the tree view
-        """
-        listview.append([job['repository'], int(job['id']), float(job['number'])])
-
-
-    def on_showBuildButton_clicked(self, widget):
-        selection = self.buildsTree.get_selection()
-        (model, iter) = selection.get_selected()
-        #Check if there is any row selected
-        if not iter:
-            w = ShowDialog("Warning!", "No build selected.")
-            w.dialog.show()
-        else:
-            buildID = model.get_value(iter, 1)
-            sbl = ShowBuildLog(buildID, model.get_value(iter, 0), self.owner)
-            sbl.w.show()
-
-
-    def gtk_main_quit(self, widget):
-        gtk.main_quit()
-
-
-class ShowBuildLog:
-
-    def __init__(self, buildID, repo, owner):
-        self.builder = gtk.Builder()
-        self.builder.add_from_file("UI/showBuildLog.glade")
-        self.builder.connect_signals(self)
-        self.buildID = buildID
-
-        self.w = self.builder.get_object("window1")
-        self.log = self.builder.get_object("logView")
-        self.log.modify_base(gtk.STATE_NORMAL, gtk.gdk.color_parse('black'))
-        self.log.modify_text(gtk.STATE_NORMAL, gtk.gdk.color_parse('white'))
-        self.logBuffer = self.builder.get_object("logBuffer")
-
-        job = travisParser.getRelatedJob(buildID, repo, owner)
-        self.logBuffer.set_text(job['log'])
-
-
-    def on_backButton_clicked(self, widget):
-        self.w.hide()
-
-
-    def on_saveButton_clicked(self, widget):
-        self.saveDialog = gtk.FileChooserDialog(title="Save To File",action=gtk.FILE_CHOOSER_ACTION_SAVE, buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_SAVE,gtk.RESPONSE_OK))
-        self.saveDialog.set_current_name(str(self.buildID) + '.log')
-        self.saveDialog.set_do_overwrite_confirmation(True)
-
-        path = ""
-        if self.saveDialog.run() == gtk.RESPONSE_OK:
-            path = self.saveDialog.get_filename()
-            f = open(path, 'w')
-            f.write(str(self.logBuffer.get_text(self.logBuffer.get_start_iter(), self.logBuffer.get_end_iter())).rstrip("\r\n"))
-            f.close()
-
-        self.saveDialog.destroy()
-
-
-if __name__ == "__main__":
-    a = TravisCIUsername()
-    a.mainWindow.show()
-    gtk.main()
+if __name__ == '__main__':
+    #Parse arguments, and execute corresponding functions
